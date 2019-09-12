19df6a0272deb828645e5b9769189c4e53038496
Jared Spears <jspears2@zagmail.gonzaga.edu>
2019-09-11 19:42:27 -0700

    hi

--- 2.13 8 ball/ViewController.swift
+++ 2.13 8 ball/ViewController.swift
@@ -9,12 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var viewer: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    
+    var index = -1
+    var newIndex = -1
+    var randomStart = Int.random(in: 0...5)
+    var answers = ["Without a doubt",
+                    "Outlook not so good",
+                    "It is certain",
+                    "Ask again later",
+                    "Signs point to yes"]
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func button(_ sender: UIButton) {
+        if randomStart > answers.count-1 {
+            randomStart = 0
+        }
+        label.text = answers[randomStart]
+        viewer.image = UIImage(named: "img\(randomStart)")
+        randomStart += 1
+        
+        
+        
+        
+        
+    }
+    
 }
 
